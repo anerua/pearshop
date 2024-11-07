e52ba3d8437c0f321f62907107646ff08d2579cb
anerua <aneruamartins@gmail.com>
2024-11-07 23:32:08 +0100

    feat: add register and login routes, views and serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import RegisterUserSerializer
+
+
+class RegisterUserAPIView(CreateAPIView):
+
+    serializer_class = RegisterUserSerializer
+    queryset = User.objects.all()
