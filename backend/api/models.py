9306daeda31e83b89d3231bca72cebb928ed9315
anerua <aneruamartins@gmail.com>
2024-11-08 09:49:48 +0100

    feat: add authentication to API routes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 
 class TrackingModel(models.Model):
@@ -29,3 +30,4 @@ class Order(TrackingModel):
     delivery_address = models.CharField(_("Delivery address"), max_length=255)
     order_items = models.ManyToManyField("OrderItem", related_name="orderitem_orders")
     total_price = models.IntegerField(_("Total price of order"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_orders")
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
+from rest_framework.permissions import BasePermission, IsAuthenticated, IsAdminUser, SAFE_METHODS
 
 from .models import *
 from .serializers import *
 
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view=None):
+        return request.method in SAFE_METHODS
+    
 
 class RegisterUserAPIView(CreateAPIView):
 
@@ -13,35 +19,41 @@ class RegisterUserAPIView(CreateAPIView):
 
 class ListCreateProductView(ListCreateAPIView):
 
+    permission_classes = [IsAdminUser|ReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
 class GetUpdateDeleteProductView(RetrieveUpdateDestroyAPIView):
 
+    permission_classes = [IsAdminUser|ReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
 class ListCreateOrderItemView(ListCreateAPIView):
 
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderItemSerializer
     queryset = OrderItem.objects.all()
 
 
 class GetOrderItemView(RetrieveAPIView):
 
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderItemSerializer
     queryset = OrderItem.objects.all()
 
 
 class ListCreateOrderView(ListCreateAPIView):
 
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
 
 
 class GetOrderView(RetrieveAPIView):
 
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
