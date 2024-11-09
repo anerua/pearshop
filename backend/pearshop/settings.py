d7f985f934d4aabca70fa3179c0426995a5df555
anerua <aneruamartins@gmail.com>
2024-11-09 17:38:52 +0100

    chore: remove axios and set access token lifetime to 15 minutes

--- backend/pearshop/settings.py
+++ backend/pearshop/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
